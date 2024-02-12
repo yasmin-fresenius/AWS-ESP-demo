923e6c249ea483db3a0ee24e84cae8c5b77fe232
Yasmin Iqbal <yasmin.iqbal1@freseniusmedicalcare.com>
2024-02-12 16:06:49 +0530

    event trigger added.

--- lambda_function.py
+++ lambda_function.py
@@ -4,13 +4,11 @@ from service_files.anonymization import process_file
 
 
 def lambda_handler(event, context):
-    # # get the bucket name from event
-    # bucket_name = event['Records'][0]['s3']['bucket']['name']
-    #
-    # # get the object key from event
-    # object_key = event['Records'][0]['s3']['object']['key']
-    bucket_name = 'esp-demo-raw-bucket'
-    object_key = 'raw/employee_config.csv'
+    # get the bucket name from event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+
+    # get the object key from event
+    object_key = event['Records'][0]['s3']['object']['key']
 
     # call the process method
     process_file(bucket_name, object_key)
